9710bdffd6c89cfe2d31f7ba081c565b70896596
Admin <sisodia.shweta1985@gmail.com>
2023-01-31 18:15:10 +0000

    new details

--- scorecardapp.py
+++ scorecardapp.py
@@ -16,3 +16,11 @@ mycursor.execute("Drop Table ScoreCardList")
 mycursor.execute("CREATE TABLE ScoreCardlist(ID int, USERNAME Varchar(20), TESTSCORE int, TEST_PCT DECIMAL(3,2), SUBID int)")
 mycursor.execute("Drop Table IF EXISTS SubjectList")
 mycursor.execute("CREATE TABLE Subjectlist(ID int, SubjectName Varchar(20))")
+
+username = str(input("Enter username:"))
+userscore = int(input("Enter Score (between 1 to 100) :"))
+score_pct=((userscore/100)*100)
+
+print("username is: " + username)
+print(" Score is: ", userscore)
+print(" Score percentage is:", score_pct,"%")
