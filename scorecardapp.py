37a595dead79b61831aea2f32198878799bf1c0c
Admin <sisodia.shweta1985@gmail.com>
2023-01-31 20:17:02 +0000

    more info added

--- scorecardapp.py
+++ scorecardapp.py
@@ -1,21 +1,16 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Pa$$w0rd"
-)
-
-print(mydb)
-
+mydb = mysql.connector.connect(host="localhost",user="root",password="Pa$$w0rd")
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE DATABASE IF NOT EXISTS scorecard1")
-mycursor.execute("use scorecard1")
-mycursor.execute("Drop Table ScoreCardList")
-mycursor.execute("CREATE TABLE ScoreCardlist(ID int, USERNAME Varchar(20), TESTSCORE int, TEST_PCT int, SUBID int)")
-mycursor.execute("Drop Table IF EXISTS SubjectList")
-mycursor.execute("CREATE TABLE Subjectlist(ID int, SubjectName Varchar(20))")
+#mycursor.execute("CREATE DATABASE IF NOT EXISTS scorecard")
+mycursor.execute("use scorecard")
+#mycursor.execute("CREATE TABLE SubjectList(ID int NOT NULL AUTO_INCREMENT,SubjectName varchar(20),PRIMARY KEY(ID))")
+#mycursor.execute("CREATE TABLE ScoreList(ID int NOT NULL AUTO_INCREMENT,UserName varchar(20),ScoreValue int,ScorePct Decimal(3,2),SUB_ID int,PRIMARY KEY(ID),FOREIGN KEY (SUB_ID) REFERENCES SubjectList(ID))")
+#mycursor.execute("INSERT INTO SubjectList (SubjectName) VALUES('ICT')")
+#mycursor.execute("INSERT INTO SubjectList (SubjectName) VALUES('MATH')")
+#mycursor.execute("INSERT INTO SubjectList (SubjectName) VALUES('CHEMISTRY')")
+#mycursor.execute("select * from SubjectList")
 
 #username = str(input("Enter username:"))
 #userscore = int(input("Enter Score (between 1 to 100) :"))
@@ -26,5 +21,7 @@ mycursor.execute("CREATE TABLE Subjectlist(ID int, SubjectName Varchar(20))")
 #print(" Score percentage is:", score_pct,"%")
 #print(" record for ", username,"updated in database")
 #print("INSERT INTO ScoreCardlist(ID, USERNAME, TESTSCORE, TEST_PCT, SUBID) VALUES(1,'Manohar',89,89,11)")
-mycursor.execute("INSERT INTO ScoreCardlist(ID, USERNAME, TESTSCORE, TEST_PCT, SUBID) VALUES(1,'Manohar',89,78,11)")
+print("INSERT INTO ScoreList (UserName, ScoreValue, ScorePct, SUB_ID) VALUES('Manohar', 50, 15.67, 1)")
+mycursor.execute("INSERT INTO ScoreList (UserName, ScoreValue, ScorePct, SUB_ID) VALUES('Manohar', 50, 15.67, 1)")
 #print("INSERT INTO ScoreCardlist(ID, USERNAME, TESTSCORE, TEST_PCT, SUBID) VALUES(1,'",username,"',", userscore, ",", score_pct, ",", "11)")
+mycursor.execute("commit")
